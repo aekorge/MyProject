9d76fb184c39fce9cffee9bb9b1893306b9ecc8a
Amar Ekorge <aekorge@gmail.com>
2021-12-24 09:49:58 +0530

    MyClass file is commited

---
+++ MyStoreProject1/src/main/java/com/mystore/pageobjects/MyClass.java
@@ -0,0 +1,20 @@
+package com.mystore.pageobjects;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/**
+		 * select value from DD by using value
+		 * 
+		 * @param locator     : Action to be performed on element (Get it from Object
+		 *                    repository)
+		 * 
+		 * @param value       : Value wish to select from dropdown list.
+		 * 
+		 * @param locatorName : Meaningful name to the element (Ex:Year Dropdown, items
+		 */   
+	
+		System.out.println("Hello");
+	}
+}
